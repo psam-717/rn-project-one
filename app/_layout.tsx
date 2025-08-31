b9ffa530eb190c6348b2110b7f0901856ba0af83
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-08-31 05:07:24 +0000

    modified the file to make it dark and ligh theme enabled

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,21 @@
 import {Stack} from 'expo-router';
 import "../global.css"
+import { useColorScheme } from 'react-native';
+import { Colors } from '../constants/Colors';
 
 // main entry point of the app
 export default function RootLayout(){
+    const colorScheme = useColorScheme() ?? 'light';
+    const theme = Colors[colorScheme];
+
     return (
-        <Stack>
-            <Stack.Screen name='index' options={{title: 'Home page'}}/>
+        <Stack screenOptions={{
+            headerStyle: {backgroundColor: theme.navBackground},
+            headerTintColor: theme.title
+        }}>
+            <Stack.Screen name='index' options={{title: 'Home page',}}/>
             <Stack.Screen name='about' options={{title: 'About page'}}/>
+            <Stack.Screen name='profile' options={{title: 'Profile page'}}/>
         </Stack>
     )
 }
