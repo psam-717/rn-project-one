6becd99354ee70a9765c5df1ea0c722aa21ac752
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-08-31 05:09:38 +0000

    a new profile page

---
+++ app/profile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>profile</Text>
+    </View>
+  )
+}
+
+export default profile
+
+const styles = StyleSheet.create({})
