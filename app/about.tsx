54b04363a748b5a4d53919285714f4643275532b
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-09-02 22:25:04 +0000

    about page template

--- app/about.tsx
+++ app/about.tsx
@@ -3,14 +3,17 @@ import React from 'react'
 import { Link } from 'expo-router'
 import ThemedView from '../components/ThemedView'
 import ThemedText from '../components/ThemedText'
+import Spacer from '../components/Spacer'
 
-const home = () => {
+const About = () => {
   return (
     <ThemedView style={[{flex: 1, justifyContent: 'center', alignItems: 'center'}]}>
-      <ThemedText title>Welcome to the about page</ThemedText>
+      <ThemedText title>How do you dos</ThemedText>
+
+      <Spacer/>
 
       <Link href='profile' >
-        <ThemedText>Profile pages</ThemedText>
+        <ThemedText>Profile page</ThemedText>
       </Link>
 
        
@@ -20,4 +23,4 @@ const home = () => {
   )
 }
 
-export default home
+export default About
